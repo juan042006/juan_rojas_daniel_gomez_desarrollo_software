3e0ce0f0e1ef5d89bb33898cc8bf893a68c837ed
juan042006 <rojas.juan.3958@eam.edu.co>
2025-02-28 11:17:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+        public void metodoA () {
+            System.out.println("Ejecutando método A");
+        }
+        public void metodoB () {
+            System.out.println("Ejecutando método B");
+        }
+        public void metodoC () {
+            System.out.println("Ejecutando método C");
+        }
+        public void metodoD () {
+            System.out.println("Ejecutando método D");
+
+            
+        }
+    }
