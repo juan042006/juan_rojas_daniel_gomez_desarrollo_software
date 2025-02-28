a0adf632f44b82329bcba86a858a508f74a501f0
juan042006 <rojas.juan.3958@eam.edu.co>
2025-02-28 11:39:50 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,43 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-        public void metodoA () {
-            System.out.println("Ejecutando método A");
-        }
-        public void metodoB () {
-            System.out.println("Ejecutando método B");
-        }
-        public void metodoC () {
-            System.out.println("Ejecutando método C");
-        }
-        public void metodoD () {
-            System.out.println("Ejecutando método D");
+git flow init
 
-            
+git checkout develop
+git pull origin develop
+git flow feature start metodoA
+git add .
+git commit -m "Implementado método A"
+git push origin feature/metodoA
+
+git checkout develop
+git pull origin develop
+git flow feature start metodoB
+git add .
+git commit -m "Implementado método B"
+git push origin feature/metodoB
+
+git checkout develop
+git pull origin develop
+git flow feature start metodoC
+git add .
+git commit -m "Implementado método C"
+git push origin feature/metodoC
+
+git checkout develop
+git pull origin develop
+git flow feature start metodoD
+git add .
+git commit -m "Implementado método D"
+git push origin feature/metodoD
+
+git checkout develop
+git pull origin develop
+git flow feature finish metodoA
+git flow feature finish metodoB
+git flow feature finish metodoC
+git flow feature finish metodoD
+git push origin develop
+
         }
     }
